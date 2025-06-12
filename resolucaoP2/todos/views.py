6f8622c2f2e1c4c1ce99bab1d73a83a96abf81ec
maiconnDutra <unifacul0@gmail.com>
2025-06-12 19:58:53 -0300

    Resolução da Questão 3

--- resolucaoP2/todos/views.py
+++ resolucaoP2/todos/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from todos.models import Todo
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 # Create your views here.
 class TodoListView(ListView):
     model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo 
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
