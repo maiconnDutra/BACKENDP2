c73268dd178bdb602755578b8e02321a78bfc951
maiconnDutra <unifacul0@gmail.com>
2025-06-12 19:50:43 -0300

    Resolução da Questão 2

--- resolucaoP2/todos/views.py
+++ resolucaoP2/todos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from todos.models import Todo
+from django.views.generic import ListView
 
 # Create your views here.
+class TodoListView(ListView):
+    model = Todo
